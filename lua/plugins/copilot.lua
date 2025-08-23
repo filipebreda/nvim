d84b050dd064cd5a878699d680f2da55ccba30be
Filipe Breda <filipe@tem.energy>
2025-08-23 09:03:35 +0100

    adjust copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,32 @@
 return {
-  'github/copilot.vim',
-  setup = function()
-    vim.g.copilot_filetypes = {
-      ['copilot-chat'] = false, -- Disable for all file types by default
-      ['oil'] = false, -- Disable for all file types by default
-    }
-  end,
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      prompts = {
+        Rename = {
+          prompt = 'Please rename the variables correctly in given selection based on context',
+          mapping = '<leader>cn',
+          selection = function(source)
+            local select = require('CopilotChat.select')
+            return select.visual(source)
+          end,
+        },
+      },
+    },
+    keys = {
+      { '<leader>cc', ':CopilotChat<CR>', mode = 'n', desc = '[C]opilot [C]hat' },
+      { '<leader>cd', ':CopilotChatDoc<CR>', mode = 'v', desc = '[C]opilot generate [D]ocstring' },
+      { '<leader>ce', ':CopilotChatExplain<CR>', mode = 'v', desc = '[C]opilot [E]xplain' },
+      { '<leader>co', ':CopilotChatOptimize<CR>', mode = 'v', desc = '[C]opilot [O]ptimize' },
+      { '<leader>cr', ':CopilotChatReview<CR>', mode = 'v', desc = '[C]opilot [R]eview' },
+      { '<leader>ct', ':CopilotChatTests<CR>', mode = 'v', desc = '[C]opilot generate [T]ests' },
+      { '<leader>cf', ':CopilotChatFix<CR>', mode = 'v', desc = '[C]opilot [F]ix code' },
+    },
+    lazy = false,
+  },
 }

a99b087c57d7bd9add17061e5e98669447d4eb2e
Filipe Breda <filipe@tem.energy>
2025-09-14 17:48:05 +0100

    update conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -35,16 +35,12 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
-      python = { 'ruff' },
+      python = { 'ruff_fix', 'ruff_organize_imports', 'ruff_format', lsp_format = 'first' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
       json = { 'jq' },
     },
-    formatters = {
-      black = {
-        prepend_args = { '--line-length', '120' },
-      },
-    },
+    formatters = {},
   },
 }

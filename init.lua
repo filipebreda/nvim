b7f7b5ee78627506c128b6fe31b0e4ddd6d181ab
Filipe Breda <filipe@tem.energy>
2025-10-19 18:33:25 +0100

    clean up init

--- init.lua
+++ init.lua
@@ -1,13 +1,1 @@
--- Set custom pre load project config
-local project_pre_config = vim.fn.getcwd() .. '/.nvim.pre.lua'
-if vim.fn.filereadable(project_pre_config) == 1 then
-  dofile(project_pre_config)
-end
-
 require 'config.lazy'
-
--- Set custom post load project config
-local project_post_config = vim.fn.getcwd() .. '/.nvim.post.lua'
-if vim.fn.filereadable(project_post_config) == 1 then
-  dofile(project_post_config)
-end

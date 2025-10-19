3e3576cd31f68e1726607ae313d1d7e258d2086e
Filipe Breda <filipe@tem.energy>
2025-10-19 18:39:55 +0100

    re-add status line well configured

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,6 +11,38 @@ return {
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
 
+    -- Simple and easy statusline.
+    --  You could remove this setup call if you don't like it,
+    --  and try some other statusline plugin
+    local statusline = require 'mini.statusline'
+    -- set use_icons to true if you have a Nerd Font
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+
+    -- You can configure sections in the statusline by overriding their
+    -- default behavior. For example, here we set the section for
+    -- cursor location to LINE:COLUMN
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+
+    statusline.config.content.active = function()
+      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+      local filename     = statusline.section_filename({ trunc_width = 140 })
+      local git          = statusline.section_git({ trunc_width = 75 })
+      local diagnostics  = statusline.section_diagnostics({ trunc_width = 75 })
+      local fileinfo     = statusline.section_fileinfo({ trunc_width = 120 })
+      local location     = statusline.section_location({ trunc_width = 75 })
+
+      return statusline.combine_groups({
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineFilename', strings = { filename } },       -- filename first
+        { hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } }, -- git + diagnostics
+        '%=',
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = 'MiniStatuslineLocation', strings = { location } },
+      })
+    end
     -- Add/delete/replace surroundings (bracets, quotes, etc.)
     --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
@@ -18,7 +50,7 @@ return {
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
-    require('mini.move').setup(config)
+    require('mini.move').setup()
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim

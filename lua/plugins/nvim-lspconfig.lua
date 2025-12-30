52ee0bd626e1464e466450bb9065c5b26a747375
Filipe Breda <filipe@tem.energy>
2025-12-30 10:09:34 +0000

    fix lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -238,10 +238,15 @@ return {
 
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
-    vim.list_extend(servers, {
+    -- filter out servers that are not available in mason registry
+    local ensure_installed = vim.tbl_keys(servers or {})
+
+    -- You can add other tools here that you want Mason to install
+    -- for you, so that they are available from within Neovim.
+    vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
     })
-    require('mason-tool-installer').setup { ensure_installed = servers }
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
       handlers = {

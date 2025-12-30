2b08896dea92aa257283a6399c0ba9533c258277
Filipe Breda <filipe@tem.energy>
2025-12-30 10:14:43 +0000

    remove telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require 'config.basic'
+require 'config.core'
 
 -- Setup custom plugins
 local backtowork = require 'custom.plugins.backtowork'
